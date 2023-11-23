82d409be431e2aec066006997e7b4444c31951a2
Randy46 <randy21nov2003@gmail.com>
2023-11-23 20:03:47 +0800

    memeprbaiki toggle utama

--- lampu.js
+++ lampu.js
@@ -1,7 +1,7 @@
 function saklar() {
-    let toggle1 = document.getElementById("default-toggle1")
-    let toggle2 = document.getElementById("default-toggle2")
-    let toggle3 = document.getElementById("default-toggle3")
+    let toggle1 = document.getElementById("toggle1")
+    let toggle2 = document.getElementById("toggle2")
+    let toggle3 = document.getElementById("toggle3")
 
     let lampu1 = document.getElementById("lampu1")
     let lampu2 = document.getElementById("lampu2")
@@ -26,18 +26,35 @@ function saklar() {
     }
 }
 
+// untuk memberi validasi toggle utama terhadap toggle1,2,3 //
+if (toggle1.checked && toggle2.checked && toggle3.checked) {
+    toggleU.checked = true
+} else {
+    toggleU.checked = false
+}
+
 function saklar1() {
-    let toggle4 = document.getElementById("default-toggle4")
+    let toggle1 = document.getElementById("toggle1")
+    let toggle2 = document.getElementById("toggle2")
+    let toggle3 = document.getElementById("toggle3")
+    let toggleU = document.getElementById("toggleU")
 
     let lampu1 = document.getElementById("lampu1")
     let lampu2 = document.getElementById("lampu2")
     let lampu3 = document.getElementById("lampu3")
 
-    if (toggle4.checked) {
+
+    if (toggleU.checked) {
+        toggle1.checked = true
+        toggle2.checked = true
+        toggle3.checked = true
         lampu1.src = 'asset/image/on.gif'
         lampu2.src = 'asset/image/on.gif'
         lampu3.src = 'asset/image/on.gif'
     } else {
+        toggle1.checked = false
+        toggle2.checked = false
+        toggle3.checked = false
         lampu1.src = 'asset/image/off.gif'
         lampu2.src = 'asset/image/off.gif'
         lampu3.src = 'asset/image/off.gif'
